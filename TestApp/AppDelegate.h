d36e8066e70ad08dbce44a51ccbf6f63a6ac422d
vinsi <leo.winc@gmail.com>
2017-09-25 15:21:20 +0530

    Initial Commit

---
+++ TestApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestApp
+//
+//  Created by vinsi on 9/25/17.
+//  Copyright © 2017 vinsi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestApp
+//
+//  Created by vinsi on 9/25/17.
+//  Copyright © 2017 vinsi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
